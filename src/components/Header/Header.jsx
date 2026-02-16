23c12eb82789ba878a4c4c7f960314582e40b83e
Nixa Zaiter <linkclone30@gmail.com>
2026-02-15 22:57:32 -0600

    Remove unused images Small refactor on Header and ItemModal

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,6 @@
 import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.png";
 function Header({ handleAddClick, weatherData }) {
   const currentDate = new Date().toLocaleString("default", {
     month: "long",
@@ -8,11 +10,7 @@ function Header({ handleAddClick, weatherData }) {
     <>
       <section className="header">
         <div className="header__container">
-          <img
-            className="header__logo"
-            src="./src/assets/Logo.svg"
-            alt="WTWR logo"
-          />
+          <img className="header__logo" src={logo} alt="WTWR logo" />
           <p className="header__local-time">
             {currentDate}, {weatherData.city}
           </p>
@@ -26,11 +24,7 @@ function Header({ handleAddClick, weatherData }) {
             + Add clothes
           </button>
           <p className="header__user-name">Terrence Tegegne</p>
-          <img
-            src="./src/assets/avatar.png"
-            alt="Avatar"
-            className="header__avatar"
-          />
+          <img src={avatar} alt="Avatar" className="header__avatar" />
         </div>
       </section>
     </>
