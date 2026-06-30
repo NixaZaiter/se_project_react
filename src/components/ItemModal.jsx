34cb8e6088d7b2581538836b79b0283a07d4ddb7
Nixa Zaiter <linkclone30@gmail.com>
2026-06-30 18:32:57 -0500

    Feat: Add currentUser context logic to deletion modal

--- src/components/ItemModal.jsx
+++ src/components/ItemModal.jsx
@@ -1,4 +1,6 @@
+import { useContext } from "react";
 import closeIcon from "../assets/close-icon-dark.svg";
+import { CurrentUserContext } from "../contexts";
 
 export default function ItemModal({
   isOpen,
@@ -6,6 +8,8 @@ export default function ItemModal({
   onClose,
   openConfirmationModal,
 }) {
+  const currentUser = useContext(CurrentUserContext);
+  const isOwn = card.owner === currentUser._id;
   const handleConfirmation = () => {
     openConfirmationModal(card);
   };
@@ -19,9 +23,11 @@ export default function ItemModal({
           <img src={closeIcon} alt="close icon" />
         </button>
         <img src={card.imageUrl} alt={card.name} className="modal__img" />
-        <button onClick={handleConfirmation} className="modal__delete-btn">
-          Delete item
-        </button>
+        {isOwn && (
+          <button onClick={handleConfirmation} className="modal__delete-btn">
+            Delete item
+          </button>
+        )}
         <div className="modal__footer">
           <h2 className="modal__caption">{card.name}</h2>
           <p className="modal__weather">Weather : {card.weather}</p>
